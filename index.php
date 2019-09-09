093df306f5ae41c3557cf28337578a923c5a3c3e
Chanankorn Jandaeng <cjundang@gmail.com>
2019-09-09 16:57:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -14,25 +14,65 @@
     <h1>Hello, world!</h1>
     <button id="btnClick"> click </button>
 
+    <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+
+
+
+
+
+
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+
   </body>
 
 
   <script>
-      function btnClick_click(){
-        alert("OK");
-      }
+      
+      function plot (dataPoints) {
+    
+    var chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      theme: "light2",
+      title:{
+        text: "Simple Line Chart"
+      },
+      axisY:{
+        includeZero: false
+      },
+      data: [{        
+        type: "line",       
+        dataPoints: dataPoints
+      }]
+    });
+    chart.render();
+    
+    }
+
+
+
       $(()=>{
-        $("#btnClick").click(btnClick_click);
+        $("button").click(function(){
+            let urlAPI = "https://api.thingspeak.com/channels/860688/feeds.json?results=50";
+            $.get(urlAPI, function(data, status){
+               // alert("Data: " + data + "\nStatus: " + status);
+                console.log(JSON.stringify(data.feeds));
+                var dataPoints = [];
+                plot(dataPoints);
+            });
+        });
+
       });  
   </script>
 
 
-
+<script>
+    
+    </script>
 
 
 
